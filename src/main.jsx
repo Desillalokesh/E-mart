a11cdda4620c82fb7979e69720a24353e1585c28
Your Name <lokeshdesilla038@gmail.com>
2024-12-24 13:46:27 +0530

    six commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import {  HashRouter } from 'react-router-dom'
 import { CartProvider } from './stores/context/CartContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<BrowserRouter basename='/portfolio'>
+<HashRouter basename='/portfolio'>
   <React.StrictMode>
     <CartProvider>
     <App />
     </CartProvider>
   </React.StrictMode>,
-</BrowserRouter>
+</HashRouter>
 )
